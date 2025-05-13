0420da32bbd88319ef2deb85a50d34bd3d8bc545
yaseer <77yaseer@example.com>
2025-05-13 15:46:35 +0300

    رفعت تطبيق حاسبه بسيطه في قت هب

---
+++ Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class clsCalculator
+    {
+        private int _Reselt=0;
+        private int _listReselt=0;
+
+        private void ListReselt()
+        {
+
+             _listReselt = _Reselt;
+        }
+        public void Add(int num)
+        {
+            ListReselt();
+            _Reselt += num;
+        }
+
+        public void Subtract(int num)
+        {
+            ListReselt();
+             _Reselt -= num;
+        }
+
+        private bool numis1(int num)
+        {
+            return num == 0;
+        }
+        public void Divide(int num)
+        {
+            ListReselt();
+            if (numis1(num))
+            {
+                 _Reselt /= 1;
+            }else 
+             _Reselt /= num;
+
+        }
+
+        public void Multiply(int num)
+        {
+            ListReselt();
+            _Reselt *= num;
+        }
+
+        public void Clear()
+        {
+            ListReselt();
+            _Reselt = 0;
+        }
+
+        public void PrintResult()
+        {
+            Console.Write(_Reselt.ToString()+"\n");
+        }
+        public void PrintListReselt()
+        {
+            Console.Write(_listReselt.ToString()+"\n");
+
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp10;
+namespace ConsoleApp10
+{
+   
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsCalculator C = new clsCalculator();
+
+            C.Clear();
+
+            C.Add(10);
+            C.PrintResult();
+
+            C.Add(100);
+            C.PrintResult();
+
+            C.Subtract(20);
+            C.PrintResult();
+
+            C.PrintListReselt();
+
+            C.Divide(0);
+            C.PrintResult();
+
+            C.Divide(2);
+            C.PrintResult();
+
+            C.Multiply(3);
+            C.PrintResult();
+
+            C.Clear();
+            C.PrintResult();
+
+            Console.ReadLine();
+        }
+    }
+}
